b79c107e18c1dcd2d2bc377f7e9c9251b4efe20a
Saurav4359 <isauravsharmaokay4359@gmail.com>
2025-09-27 01:48:16 +0530

    changed the code for Todo

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
- 
+ const text=document.getElementById("text");
+ const button=document.getElementById("AddTask");
+ const tasklist=document.getElementById("tasklist");
+
+
+ button.addEventListener("click",() => {
+    const taskValue=text.value.trim();
+    if(!taskValue)
+        return alert("Enter the Task first");
+
+    const li=document.createElement("li");
+  li.textContent=taskValue;
+
+  tasklist.append(li);
+  text.value="";
+
+
+ });
