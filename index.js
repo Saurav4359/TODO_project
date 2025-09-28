1ec75d005eb059323cecfde8fcaa9658907592df
Saurav4359 <isauravsharmaokay4359@gmail.com>
2025-09-29 03:18:48 +0530

    updated the code and TODO is ready

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-  const textValue=document.getElementById("textValue");
- const AddTask=document.getElementById("AddTask");
- const taskList=document.getElementById("taskList");
-
+  const textValue=document.querySelector("input");
+ const AddTask=document.querySelector("button");
+ 
+let ctr=1;
 
  function call() {
-    const text=textValue.value;
+    const text=textValue.value.trim();
     if(!text){
   return alert("Enter the task");
   
     }
        
-
-      const newNode=document.createElement("li");
-      
-      taskList.appendChild(newNode);
+       
+      const newNode=document.createElement("div");
+      newNode.setAttribute("id","todo"+ctr);
+       newNode.className="task-item";
    
-       newNode.textContent=text;
-        
-       textValue.value="";
-      const deleteButton=document.createElement("button");
-     newNode.appendChild(deleteButton);
-     deleteButton.textContent="delete";
-     
+       newNode.innerHTML="<div>"+ text +"</div><button onclick='deleteTask("+ctr+")'>Delete</button>" ;
+        textValue.value="";
+        document.querySelector("body").appendChild(newNode);
+ 
+    
+     ctr++;
  }
  
  AddTask.addEventListener("click",call);
  textValue.addEventListener("keypress",(e)=>
 {
-    if(e.key==="Enter")
+    if(e.key==="Enter") 
     {
         call();
     }
 })
 
-function deleteTask() {
+function deleteTask(ctr) {
 
-    
+const element=document.getElementById("todo"+ctr);
+element.parentNode.removeChild(element);
     
 }
 
-//AddTask.addEventListener("click",deleteTask);
+ 
